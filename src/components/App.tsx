9d07e131526174865d034ca9cfffd73e46da554d
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-04-10 15:34:40 +0200

    :pencil: localization

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,9 @@
 import React, { Suspense } from "react";
-import "./App.scss";
 import { Provider } from "react-redux";
 import store from "../util/store";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { ErrorBoundary } from "./ErrorBoundary";
+import "../util/i18n";
 
 const Home = React.lazy(() => import("../pages/Home"));
 const Login = React.lazy(() => import("../pages/Login"));
@@ -12,7 +12,7 @@ function App() {
 	return (
 		<Provider store={store}>
 			<ErrorBoundary>
-				<Suspense fallback={<div>Loading ...</div>}>
+				<Suspense fallback={<div className="text">Loading ...</div>}>
 					<Router>
 						<Route exact path="/login" component={Login}></Route>
 						<Route exact path="/" component={Home}></Route>
