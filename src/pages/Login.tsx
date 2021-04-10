7259d27fac4d6eb594d47619a39378c439ce0204
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-04-11 01:48:28 +0200

    :sparkles: Login Page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,37 @@
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import Button from "../components/Button";
 import Input from "../components/Input";
 import "./Login.scss";
 
 export default function LoginScreen() {
+	const { t } = useTranslation("login");
+
 	return (
 		<div className="page login">
-			<Link to="/">Home</Link>
-			<p className="text">Loginscreen</p>
+			<form className="form">
+				<h1 className="text headline">{t("login")}</h1>
 
-			<Input labelText="test"></Input>
+				{/* email or phone autocomplete */}
+				<Input autoComplete="email" type="text" className="emailPhone" labelText={t("emailTelephone")}></Input>
+
+				<Input
+					className="password"
+					type="password"
+					labelText={t("password")}
+					autoComplete="current-password"
+				></Input>
+
+				{/* // TODO instance selection */}
+
+				<Link to="/resetPassword">{t("forgotPassword")}</Link>
+
+				<Button className="submit " primary>
+					{t("login")}
+				</Button>
+
+				<Link to="/register">{t("registerNotice")}</Link>
+			</form>
 		</div>
 	);
 }
