1d529c94c4229d96a75d118f8dd5b2cfb2ff6e22
afeuerstein <32029275+afeuerstein@users.noreply.github.com>
2021-04-17 20:21:01 +0200

    changed stylesheets, created translations

--- src/pages/Instance.tsx
+++ src/pages/Instance.tsx
@@ -2,32 +2,34 @@ import { Icon } from "../framework/Icon";
 import { Input } from "../framework/Input";
 import { List, ListItem } from "../framework/List";
 import { Text } from "../framework/Text";
+import { useTranslation } from "react-i18next";
 import "./Instance.scss";
 
 export default function Instance(props: any) {
+	const { t } = useTranslation("instance");
 	return (
 		<>
 			<div className="sidebar">
 				<List>
-					<Text headline>Discover</Text>
+					<Text headline>{t("discover")}</Text>
 					<ListItem active primary>
 						<Icon className="left" icon="compass"></Icon>
-						<div className="content">Explore</div>
+						<div className="content">{t("explore")}</div>
 					</ListItem>
 					<ListItem>
 						<Icon className="left" icon="settings"></Icon>
-						<div className="content">Settings</div>
+						<div className="content">{t("settings")}</div>
 					</ListItem>
 					<ListItem>
 						<Icon className="left" icon="plus"></Icon>
-						<div className="content">Add</div>
+						<div className="content">{t("add")}</div>
 					</ListItem>
 				</List>
 			</div>
 
 			<div className="content">
-				<Text headline>Find Instances</Text>
-				<Input placeholder="Explore communities" labelText="Search"></Input>
+				<Text headline>{t("find")}</Text>
+				<Input placeholder={t("findPlaceholder")} labelText={t("search")}></Input>
 			</div>
 		</>
 	);
