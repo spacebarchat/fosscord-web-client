e72f0facf786566a5e44a4e9e25e2cf6b2e2383e
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-04-18 02:48:08 +0200

    :sparkles: Network Select + Add Page

--- src/pages/Network.tsx
+++ src/pages/Network.tsx
@@ -5,26 +5,54 @@ import { List, ListItem } from "../framework/List";
 import { Text } from "../framework/Text";
 import { useTranslation } from "react-i18next";
 import "./Network.scss";
-import { RootState, useSelector } from "react-redux";
+import { RootState, useDispatch, useSelector } from "react-redux";
+import { Network } from "../reducers/networks";
+import { MouseEvent, useState } from "react";
+import { Button } from "../framework/Button";
+import "missing-native-js-functions";
 
 export interface NetworkPageProps {
-	onSelect?: (id: string) => any;
+	selected?: Network;
+	onSelect?: (network: Network) => any;
 }
 
 export default function NetworkPage(props: NetworkPageProps) {
 	const { t } = useTranslation("network");
+	const dispatch = useDispatch();
 	const networks = useSelector((s: RootState) => s.networks);
+	const [list, setList] = useState("explore");
+	const [url, setURL] = useState("");
+
+	function addNetwork(event: any) {
+		event.preventDefault();
+		if (!url) return;
+
+		const network = {
+			id: Math.randomIntBetween(0, 100000),
+			host: url,
+			name: url,
+		};
+		// TODO: validate and fetch network
+		dispatch({
+			type: "ADD_NETWORK",
+			payload: network,
+		});
+		// @ts-ignore
+		props.onSelect?.(network);
+		setURL("");
+		// TODO: show success toast
+	}
 
 	return (
 		<>
 			<div className="sidebar">
 				<List>
 					<Text headline>{t("discover")}</Text>
-					<ListItem active primary>
+					<ListItem state={list} setState={setList} name="explore" primary>
 						<Icon className="left" icon="compass"></Icon>
 						<div className="content">{t("explore")}</div>
 					</ListItem>
-					<ListItem>
+					<ListItem state={list} setState={setList} name="add" primary>
 						<Icon className="left" icon="plus"></Icon>
 						<div className="content">{t("add")}</div>
 					</ListItem>
@@ -32,21 +60,57 @@ export default function NetworkPage(props: NetworkPageProps) {
 			</div>
 
 			<div className="content">
-				<Text headline>{t("find")}</Text>
-				<Input placeholder={t("findPlaceholder")} labelText={t("search")}></Input>
+				{/* // TODO: search box with all public networks */}
+				{/* <Input placeholder={t("findPlaceholder")} labelText={t("search")}></Input> */}
 
-				<div className="card-grid">
-					{networks.map((x) => (
-						<Card
-							onClick={() => props.onSelect?.(x.id)}
-							title={x.name}
-							description={x.description}
-							header={x.splash}
-							icon={x.icon}
-							verified={x.verified}
-						></Card>
-					))}
-				</div>
+				{(() => {
+					switch (list) {
+						case "explore":
+							return (
+								<>
+									<Text headline>{t("chooseNetwork")}</Text>
+
+									<div className="card-grid">
+										{networks.map((x) => (
+											<Card
+												active={x === props.selected}
+												key={x.id}
+												onClick={() => props.onSelect?.(x)}
+												title={x.name}
+												description={x.description}
+												header={x.splash}
+												icon={x.icon}
+												verified={x.verified}
+											></Card>
+										))}
+									</div>
+								</>
+							);
+						case "add":
+							return (
+								<div className="addNetwork">
+									<Text headline>{t("addNetwork")}</Text>
+
+									<Text>{t("addNotice")}</Text>
+									<Text muted>{t("setupNotice")}</Text>
+
+									<div className="inputButton">
+										<Input
+											type="url"
+											value={url}
+											onChange={(e) => setURL(e.target.value)}
+											labelText="URL"
+											placeholder={t("addPlaceholder")}
+										></Input>
+
+										<Button onClick={addNetwork} primary>
+											Add
+										</Button>
+									</div>
+								</div>
+							);
+					}
+				})()}
 			</div>
 		</>
 	);
