1bf32431d22fa79552f47966f17214c0a7ef3a2a
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-04-18 02:48:20 +0200

    :sparkles: Save redux store to localStorage

--- src/util/store.ts
+++ src/util/store.ts
@@ -2,8 +2,35 @@ import { applyMiddleware, compose, createStore } from "redux";
 import reducers from "../reducers/";
 import thunk from "redux-thunk";
 
+export const loadState = () => {
+	try {
+		const serializedState = localStorage.getItem("state");
+		if (serializedState === null) {
+			return undefined;
+		}
+		return JSON.parse(serializedState);
+	} catch (err) {
+		return undefined;
+	}
+};
+
+export const saveState = (state: any) => {
+	try {
+		const serializedState = JSON.stringify(state);
+		localStorage.setItem("state", serializedState);
+	} catch {
+		// ignore write errors
+	}
+};
+
 // @ts-ignore
 const devTools = global.__REDUX_DEVTOOLS_EXTENSION__ ? global.__REDUX_DEVTOOLS_EXTENSION__() : undefined;
 const middlewares = [devTools, applyMiddleware(thunk)].filter((x) => x !== undefined);
+const persistedState = loadState();
 
-export default createStore(reducers, compose(...middlewares));
+const store = createStore(reducers, persistedState, compose(...middlewares));
+export default store;
+
+store.subscribe(() => {
+	saveState(store.getState());
+});
