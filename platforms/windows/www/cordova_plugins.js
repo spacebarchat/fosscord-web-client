cd4886a0585ddbe597e1d4b2e12e986b47a67572
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-04-22 00:56:54 +0200

    :tada: android build

---
+++ platforms/windows/www/cordova_plugins.js
@@ -0,0 +1,6 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4"
+  };
+});
--- public/cordova/ios/cordova_plugins.js
+++ public/cordova/ios/cordova_plugins.js
@@ -1,4 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports = [];
-  module.exports.metadata = {};
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4"
+  };
 });
