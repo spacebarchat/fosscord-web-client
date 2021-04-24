650cef6f1b489d85f1f8dc0590b0881d40da0843
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-04-24 12:54:53 +0200

    :bug: fix homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
+import { Text } from "../framework/Text";
 
 export default function HomeScreen(props: any) {
 	return (
 		<div>
 			<Link to="/login">Login</Link>
 			<Text>Homescreen</Text>
-			<Text>{count}</Text>
 		</div>
 	);
 }
